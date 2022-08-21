2234de2c3631301086dc75edf7f06caf21229eee
Sherwin Felton <sherwinfelton36@gmail.com>
2022-08-20 23:01:05 -0400

    apiappusinggolang

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
+
+	router.Run()
+}
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Sherwin Felton",
+		"bio":  "A Software Engineer",
+	})
+}
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Sherwin Felton",
+		"subtitle": "Golang_WEBAPP",
+	})
+}
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
