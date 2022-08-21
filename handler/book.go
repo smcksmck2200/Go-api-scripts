9338c6c0c05916f410df23b217e104c2832553c7
Sherwin Felton <sherwinfelton36@gmail.com>
2022-08-21 03:58:34 -0400

    add validator

---
+++ handler/book.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"GOLANG_WEBAPP/book"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+func RootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Sherwin Felton",
+		"bio":  "A Software Engineer",
+	})
+}
+func HelloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Hello World!",
+		"subtitle": "Golang_WEBAPP",
+	})
+}
+func BooksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func QueryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+
+func PostBooksHandler(c *gin.Context) {
+	var bookInput book.BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on Field: %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+	})
+}
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"net/http"
+	"GOLANG_WEBAPP/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,32 +9,13 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", rootHandler)
-	router.GET("/hello", helloHandler)
-	router.GET("/books/:id/:title", booksHandler)
-	router.GET("/query", queryHandler)
+	v1 := router.Group("/v1")
 
-	router.Run()
-}
-func rootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"name": "Sherwin Felton",
-		"bio":  "A Software Engineer",
-	})
-}
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"title":    "Sherwin Felton",
-		"subtitle": "Golang_WEBAPP",
-	})
-}
-func booksHandler(c *gin.Context) {
-	id := c.Param("id")
-	title := c.Param("title")
-	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
-}
-func queryHandler(c *gin.Context) {
-	title := c.Query("title")
-	price := c.Query("price")
-	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+	v1.GET("/", handler.RootHandler)
+	v1.GET("/hello", handler.HelloHandler)
+	v1.GET("/books/:id/:title", handler.BooksHandler)
+	v1.GET("/query", handler.QueryHandler)
+	v1.POST("/books", handler.PostBooksHandler)
+
+	router.Run(":3000")
 }
